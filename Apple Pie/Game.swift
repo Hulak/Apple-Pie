bcc25afabc527b19154ab2ccd78bf05985c60257
Alyona Hulak <alyonahulak@alyonas-macbook-pro.local>
2018-07-31 11:39:45 +0300

    Initial Commit

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,32 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Alyona Hulak on 7/28/18.
+//  Copyright © 2018 Alyona Hulak. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
